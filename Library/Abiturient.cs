e1684934e7733f4f0cb3d93a21ae7a222f82b99a
Maksim Penkov <ipz231_pmo@student.ztu.edu.ua>
2024-01-19 15:46:51 +0200

    Add People class

---
+++ Library/Abiturient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Abiturient : People
+    {
+        public float ZNO {  get; set; }
+        public float Atestat { get; set; }
+        public string School { get; set; }
+
+        public Abiturient (string firstName, string lastName, int day, int month, int year, float zNO, float atestat, string school) : base(firstName, lastName, day, month, year)
+        {
+            ZNO = zNO;
+            Atestat = atestat;
+            School = school;
+        }
+        public override string ShowInfo()
+        {
+            return new string($"Abiturient\n\tFirst Name: {FirstName}\n\tLast Name: {LastName}\n\tBirthday: {Birthday}\n\tZNO: {ZNO}\n\tAtestat: {Atestat}\n\tSchool: {School}\n");
+        }
+    }
+}
---
+++ Library/People.cs
@@ -0,0 +1,19 @@
+﻿namespace Library
+{
+    public class People
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Birthday { get; set; }
+        public People(string firstName, string lastName, int day, int month, int year) 
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Birthday = day.ToString() + "." + month.ToString() + "." + year.ToString();
+        }
+        public virtual string ShowInfo()
+        {            
+            return new string($"People\n\tFirst Name: {FirstName}\n\tLast Name: {LastName}\n\tBirthday: {Birthday}\n");
+        }
+    }
+}
